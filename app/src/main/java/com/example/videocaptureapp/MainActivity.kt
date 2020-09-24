a8712162a67853888fb76ba3e5fee2704c0b17eb
flyking-web <craigkamunyu@gmail.com>
2020-09-24 09:30:14 +0300

    capturing video

---
+++ app/src/main/java/com/example/videocaptureapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.videocaptureapp
+
+import android.os.Bundle
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
